d239254d82e9488d0ac1576004cf32d32def196f
Jason Derr <dharrmabum@gmail.com>
2018-08-31 10:52:32 -0700

    returning an array with the largest numbers from each of the sub array

---
+++ 05/large_number.js
@@ -0,0 +1,11 @@
+//Free Code Camp’s “Return Largest Numbers in Arrays” challenge.
+//This involves returning an array with the largest numbers from each of the sub arrays.
+
+function largestOfFour(mainArray) {
+  return mainArray.map(function (subArray){
+    return subArray.reduce(function (previousLargestNumber, currentLargestNumber) {
+      return (currentLargestNumber > previousLargestNumber) ? currentLargestNumber : previousLargestNumber;
+    }, 0);
+  });
+}
+largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
