b6211d1bf9bea5a4706acc3987509b4a461398bb
Jason Derr <dharrmabum@gmail.com>
2018-09-04 00:00:41 -0700

    When you factorialize a number, you are multiplying that number by each consecutive number minus one.

---
+++ 08/factorialize.js
@@ -0,0 +1,10 @@
+//When you factorialize a number, you are multiplying that number by each consecutive number minus one.
+function factorialize(num) {
+  if (num === 0 || num === 1)
+    return 1;
+  for (var i = num - 1; i >= 1; i--) {
+    num *= i;
+  }
+  return num;
+}
+factorialize(7);
