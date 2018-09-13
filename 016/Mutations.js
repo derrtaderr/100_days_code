67c932e9cb0e6a621c3760d79e4718e6815bdab7
Jason Derr <dharrmabum@gmail.com>
2018-09-12 18:17:01 -0700

    Basic Algorithm Scripting: Mutations

---
+++ 016/Mutations.js
@@ -0,0 +1,12 @@
+//Return true if the string in the first element
+//of the array contains all of the letters of
+//the string in the second element of the array.
+
+function mutation(arr) {
+  return arr[1].toLowerCase()
+    .split('')
+    .every(function(letter) {
+      return arr[0].toLowerCase()
+        .indexOf(letter) != -1;
+    });
+}
