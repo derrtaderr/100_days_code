06d33ae694ab533d6670816fd401c3581703ecc2
Jason Derr <dharrmabum@gmail.com>
2018-09-15 15:48:40 -0700

    Slice and Splice

---
+++ 018/slice-splice.js
@@ -0,0 +1,12 @@
+//Use the array methods slice and splice to copy each element of the first array into the second array, in order.
+//Begin inserting elements at index n of the second array.
+//Return the resulting array. The input arrays should remain the same after the function runs.
+
+function frankenSplice(arr1, arr2, n) {
+  let localArray = arr2.slice();
+  for (let i = 0; i < arr1.length; i++) {
+    localArray.splice(n, 0, arr1[i]);
+    n++;
+  }
+  return localArray;
+}
